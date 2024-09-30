0ede465797a359383633ba9996228a66e96ca991
Hauru427 <hauru.051427@gmail.com>
2024-10-01 02:21:07 +0900

    organizmの作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import './App.css'
 import { PrimaryButton } from './components/atoms/button/PrimaryButton'
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user = {
+  name: "ぽこ",
+  image: "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  email: "12345@example.com",
+  phone: "090-9999-8888",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+}
+
 
 export default function App() {
   return (
@@ -10,6 +23,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
