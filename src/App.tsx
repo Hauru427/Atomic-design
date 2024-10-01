c66aefa59dbca217703a878b41cc335948e897ae
Hauru427 <hauru.051427@gmail.com>
2024-10-01 19:01:31 +0900

    Pageの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,6 @@
 import './App.css'
-import { PrimaryButton } from './components/atoms/button/PrimaryButton'
-import { SecondaryButton } from './components/atoms/button/SecondaryButton';
-import { SearchInput } from './components/molecules/SearchInput';
-import { UserCard } from './components/organisms/user/UserCard';
-
-const user = {
-  name: "ぽこ",
-  image: "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  email: "12345@example.com",
-  phone: "090-9999-8888",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-}
-
+import { Router } from './router/Router';
 
 export default function App() {
-  return (
-    <div className='App'>
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map(((val) => {
+    return {
+        id: val,
+        name: "ぽこ-${val}",
+        image: "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        email: "12345@example.com",
+        phone: "090-9999-8888",
+        company: {
+        name: "テスト株式会社"
+        },
+        website: "https://google.com"
+    }
+}))  
+
+export const Users = () => {
+    return (
+        <SContainer>
+            <h2>ユーザー一覧</h2>
+            <SearchInput />
+            <SUserArea>
+                {users.map((user) => (
+                    <UserCard key={users.id} user={user} />
+                ))}
+            </SUserArea>
+        </SContainer>
+    )
+}
+
+const SContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
+`;
+const SUserArea = styled.div`
+    padding-top: 40px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 20px;
+`;
