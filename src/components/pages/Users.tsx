ae3c1a72dfddd342615517ddcb71e114a77bad37
Hauru427 <hauru.051427@gmail.com>
2024-10-02 16:56:27 +0900

    グローバルなステート管理を使用しないダメな例

--- src/components/pages/Users.tsx
+++ src/components/pages/Users.tsx
@@ -1,11 +1,13 @@
+import React from "react";
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
 const users = [...Array(10).keys()].map(((val) => {
     return {
         id: val,
-        name: "ぽこ-${val}",
+        name: `ぽこ${val}`,
         image: "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         email: "12345@example.com",
         phone: "090-9999-8888",
@@ -17,13 +19,15 @@ const users = [...Array(10).keys()].map(((val) => {
 }))  
 
 export const Users = () => {
+    const { state } = useLocation();
+    const isAdmin = state ? state.isAdmin : false;
     return (
         <SContainer>
             <h2>ユーザー一覧</h2>
             <SearchInput />
             <SUserArea>
                 {users.map((user) => (
-                    <UserCard key={users.id} user={user} />
+                    <UserCard key={users.id} user={user} isAdmin={isAdmin} />
                 ))}
             </SUserArea>
         </SContainer>
